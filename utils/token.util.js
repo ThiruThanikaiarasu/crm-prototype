6c5101a77bcd02ed427ec50627e52025dc7e0507
Thirunavukkarasu T <thiru.thanikaiarasu@gmail.com>
2025-11-25 00:11:38 +0530

    fix: add maxAge to JWT cookie to persist in private window

--- utils/token.util.js
+++ utils/token.util.js
@@ -13,6 +13,8 @@ const options = {
     httpOnly: true,
     secure: true,
     sameSite: 'None',
+    path: '/',
+    maxAge: 1000 * 60 * 60 * 24 * 90
 }
 
 const generateAccessToken = ({ tenantId, userId, role }) => {
