c19216c74205bcb82b20af99d176b9ddaa65ee56
Thirunavukkarasu T <thiru.thanikaiarasu@gmail.com>
2026-01-06 20:27:46 +0530

    fix(invite): add null check before cheking invite status

--- services/invite.service.js
+++ services/invite.service.js
@@ -3,8 +3,7 @@ const organizationInviteModel = require("../models/organizationInvite.model")
 const checkIfInviteAlreadyAccepted = async (tenantId, email) => {
     const OrganizationInvite = organizationInviteModel(tenantId)
     const invite = await OrganizationInvite.findOne({ email: email })
-    console.log(invite)
-    if (invite.status == 'accepted' || invite.status == 'rejected') {
+    if (invite?.status == 'accepted' || invite?.status == 'rejected') {
         return true
     }
     return false
